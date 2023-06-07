e5682f8bb9739a22e2464b3446531a3a94da74d8
Anvor <anvor.arak@karlstorz.com>
2023-06-07 23:32:51 +0300

    add week4 code

---
+++ WhileLoops/Program.cs
@@ -0,0 +1,26 @@
+﻿//ForLoop k'ivitab ja korrutab koodi senikaua kuni mingi tingimus taitub
+
+Random rnd = new Random();
+int cpuRandom;
+
+bool loopActive = true; // bool - true/false
+
+while (loopActive)
+{
+ cpuRandom = rnd.Next(1, 4);
+    Console.WriteLine($"cpu has generated{cpuRandom}.");
+    Console.WriteLine("Make a guess. Enter a number 1-3");
+
+    int userNumber = Int32.Parse(Console.ReadLine());
+
+    if (userNumber == cpuRandom)
+    {
+        Console.WriteLine("Congratulations, you won!");
+        loopActive = false;
+    }else
+    {
+        Console.WriteLine("Oops. Try again");
+    }
+}
+
+Console.WriteLine("Have a nice day!");
